62133c468ef78d1cb943287938f1dcee074c2c2b
LENG64nbnb <264569636@qq.com>
2024-12-31 20:32:06 +0800

    Add files via upload

--- 冷脚本新源码.lua
+++ 冷脚本新源码.lua
@@ -80,11 +80,10 @@ local UITab2 = win:Tab("『帮助榜』",'16060333448')
 local about = UITab2:section("『LENG Script』",true)
 
 about:Label("NO.1:月星")
-about:Label("NO.2:小皮")
+about:Label("NO.2:lishichuan丁丁")
 about:Label("NO.3:小玄奘")
 about:Label("NO.4:小云")
-about:Label("NO.5:丁丁")
-about:Label("NO.6:林")
+about:Label("NO.5:林")
 
 local UITab3 = win:Tab("『出生榜』",'16060333448')
 
@@ -660,133 +659,6 @@ end)
 
 about:Button("Dex抓包",function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoFenHG/Dex-Explorer/refs/heads/main/Dex-Explorer.lua"))()
-end)
-
-about:Button("位置仪",function()
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local title = Instance.new("TextLabel")
-local copy = Instance.new("TextButton")
-local pos = Instance.new("TextBox")
-local find = Instance.new("TextButton")
- 
---Properties:
- 
-ScreenGui.Parent = game.CoreGui
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
- 
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(43, 43, 43)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.639646292, 0, 0.399008662, 0)
-Frame.Size = UDim2.new(0, 387, 0, 206)
-Frame.Active = true
- 
-title.Name = "title"
-title.Parent = Frame
-title.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-title.BorderSizePixel = 0
-title.Size = UDim2.new(0, 387, 0, 50)
-title.Font = Enum.Font.GothamBold
-title.Text = "位置仪"
-title.TextColor3 = Color3.fromRGB(255, 255, 255)
-title.TextSize = 30.000
-title.TextWrapped = true
- 
-copy.Name = "copy"
-copy.Parent = Frame
-copy.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-copy.BorderSizePixel = 0
-copy.Position = UDim2.new(0.527131796, 0, 0.635922313, 0)
-copy.Size = UDim2.new(0, 148, 0, 50)
-copy.Font = Enum.Font.GothamSemibold
-copy.Text = "复制"
-copy.TextColor3 = Color3.fromRGB(255, 255, 255)
-copy.TextSize = 20.000
- 
-pos.Name = "pos"
-pos.Parent = Frame
-pos.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-pos.BorderSizePixel = 0
-pos.Position = UDim2.new(0.0904392749, 0, 0.305825233, 0)
-pos.Size = UDim2.new(0, 317, 0, 50)
-pos.Font = Enum.Font.GothamSemibold
-pos.Text = ""
-pos.TextColor3 = Color3.fromRGB(255, 255, 255)
-pos.TextSize = 14.000
-pos.TextWrapped = true
- 
-find.Name = "find"
-find.Parent = Frame
-find.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-find.BorderSizePixel = 0
-find.Position = UDim2.new(0.0904392898, 0, 0.635922313, 0)
-find.Size = UDim2.new(0, 148, 0, 50)
-find.Font = Enum.Font.GothamSemibold
-find.Text = "查找当前位置"
-find.TextColor3 = Color3.fromRGB(255, 255, 255)
-find.TextSize = 20.000
- 
--- Scripts:
- 
-local function UMTQ_fake_script() -- copy.LocalScript 
-	local script = Instance.new('LocalScript', copy)
- 
-	script.Parent.MouseButton1Click:Connect(function()
-		setclipboard(script.Parent.Parent.pos.Text)
-	end)
-end
-coroutine.wrap(UMTQ_fake_script)()
-local function KJAYG_fake_script() -- Frame.Dragify 
-	local script = Instance.new('LocalScript', Frame)
- 
-	local UIS = game:GetService("UserInputService")
-	function dragify(Frame)
-	    dragToggle = nil
-	    local dragSpeed = 0
-	    dragInput = nil
-	    dragStart = nil
-	    local dragPos = nil
-	    function updateInput(input)
-	        local Delta = input.Position - dragStart
-	        local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
-	        game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.25), {Position = Position}):Play()
-	    end
-	    Frame.InputBegan:Connect(function(input)
-	        if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
-	            dragToggle = true
-	            dragStart = input.Position
-	            startPos = Frame.Position
-	            input.Changed:Connect(function()
-	                if input.UserInputState == Enum.UserInputState.End then
-	                    dragToggle = false
-	                end
-	            end)
-	        end
-	    end)
-	    Frame.InputChanged:Connect(function(input)
-	        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-	            dragInput = input
-	        end
-	    end)
-	    game:GetService("UserInputService").InputChanged:Connect(function(input)
-	        if input == dragInput and dragToggle then
-	            updateInput(input)
-	        end
-	    end)
-	end
- 
-	dragify(script.Parent)
-end
-coroutine.wrap(KJAYG_fake_script)()
-local function EKBNYI_fake_script() -- find.LocalScript 
-	local script = Instance.new('LocalScript', find)
- 
-	script.Parent.MouseButton1Down:Connect(function()
-		script.Parent.Parent.pos.Text = tostring(game.Players.LocalPlayer.Character.HumanoidRootPart.Position)
-	end)
-end
-coroutine.wrap(EKBNYI_fake_script)()
 end)
 
 local UITab7 = win:Tab("『画质光影』",'16060333448')
@@ -2472,10 +2344,6 @@ about:Button("变成囚犯", function()
     workspace.Remote.TeamEvent:FireServer("Bright orange");
 end)
 
-about:Toggle("杀死光环", "SSGH", false, function(SSGH)
-    States.KillAura = SSGH if state then print("Kill Aura On") CreateKillPart() else print("Kill Aura Off") if Parts[1] and Parts[1].Name == "KillAura" then Parts[1]:Destroy() Parts[1] = nil end end end) function CreateKillPart() if Parts[1] then pcall(function() Parts[1]:Destroy() end) Parts[1] = nil end local Part = Instance.new("Part",plr.Character) local hilight = Instance.new("Highlight",Part) hilight.FillTransparency = 1 Part.Anchored = true Part.CanCollide = false Part.CanTouch = false Part.Material = Enum.Material.SmoothPlastic Part.Transparency = .98 Part.Material = Enum.Material.SmoothPlastic Part.BrickColor = BrickColor.White() Part.Size = Vector3.new(20,2,20) Part.Name = "KillAura" Parts[1] = Part end task.spawn(function() repeat task.wait()until plr.Character and char and char:FindFirstChildOfClass("Humanoid") if States.KillAura then CreateKillPart() end end) game:GetService("RunService").Stepped:Connect(function() if States.KillAura then for i,v in pairs(game.Players:GetPlayers()) do if v ~= game.Players.LocalPlayer then if (v.Character.HumanoidRootPart.Position - plr.Character.HumanoidRootPart.Position).magnitude <14 and v.Character.Humanoid.Health >0 then local args = {[1] = v} for i =1,2 do task.spawn(function() game:GetService("ReplicatedStorage").meleeEvent:FireServer(unpack(args)) end) end end end end end
-end)
-
 about:Button("手里剑秒杀",function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PSXhuge/1/114514/jian"))()
 end)
@@ -3363,19 +3231,6 @@ local about = UITab24:section("『挖到中国』",true)
 
 about:Button("远程购买炸弹",function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/odhdshhe/wachuzhongguo/refs/heads/main/jm%E6%8C%96%E5%88%B0%E4%B8%AD%E5%9B%BD%E8%BF%9C%E7%A8%8B%E8%B4%AD%E4%B9%B0%E7%82%B8%E5%BC%B9.lua"))()
-end)
-
-local UITab25 = win:Tab("『冰上钓鱼模拟器』",'16060333448')
-
-local about = UITab25:section("『冰上钓鱼模拟器』",true)
-
-about:Toggle("随机上鱼", "Shangyu", true, function(Value)
-    task.spawn(function()
-        while Value do
-            game:GetService("ReplicatedStorage").Remotes.FishGame12:FireServer(true)
-        task.wait()
-        end
-    end)
 end)
 
 local UITab26 = win:Tab("『起床战争』",'16060333448')
