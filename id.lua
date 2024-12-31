308e85699519e76733f36cefeb59e81eae064306
LENG64nbnb <264569636@qq.com>
2024-12-31 21:25:21 +0800

    Add files via upload

---
+++ id.lua
@@ -0,0 +1,34 @@
+if game.PlaceId == 6839171747 then--doors
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/QQ2368002332/refs/heads/main/Roblox-cold-script-Doors.txt"))()
+elseif game.PlaceId == 3623096087 then--力量传奇
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/QQ2368002332/refs/heads/main/Roblox-cold-script-Legend%20of%20power.txt"))()
+elseif game.PlaceId == 7239319209 then--Ohio
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/QQ2368002332/refs/heads/main/Roblox-cold-script-ohio.txt"))()
+elseif game.PlaceId == 4618049391 then--河北唐县
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/QQ2368002332/refs/heads/main/Roblox-cold-script-Tang%20County%2C%20Hebei%20Province.txt"))()
+elseif game.PlaceId == 18241512421 then-- 狗熊岭危机
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/QQ2368002332/refs/heads/main/Roblox-cold-script-Gouxiong%20Ling.txt"))()
+elseif game.PlaceId == 5256165620 then-- 火箭发射模拟器
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/QQ2368002332/refs/heads/main/Roblox-cold-script-Rocket%20launch%20simulator.txt"))()
+elseif game.PlaceId == 12931609417 then-- 颜色或死亡
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/QQ2368002332/refs/heads/main/Roblox-cold-script-Color%20or%20death.txt"))()
+elseif game.PlaceId == 110333320616502 then-- 格蕾丝
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/QQ2368002332/refs/heads/main/Roblox-cold-script-Grace.txt"))()
+elseif game.PlaceId == 138837502355157 then-- 格蕾丝
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/QQ2368002332/refs/heads/main/Roblox-cold-script-Grace.txt"))()
+elseif game.PlaceId == 13822889 then-- 伐木大亨2
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/QQ2368002332/refs/heads/main/Roblox-cold-script-Wood-chopping%20tycoon.txt"))()
+else--通用
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG64nbnb/leng-/refs/heads/main/%E5%86%B7%E8%84%9A%E6%9C%AC%E6%96%B0%E6%BA%90%E7%A0%81.lua"))()
+end
